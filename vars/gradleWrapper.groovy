81a7c301598f083b910e468aad002d6665a5c78d
Manfred Endres <manfred.endres@tslarusso.de>
2017-06-15 14:30:01 +0200

    Use isUnix step instead of system properties

--- vars/gradleWrapper.groovy
+++ vars/gradleWrapper.groovy
@@ -4,8 +4,7 @@
  * execute gradlew or gradlew.bat based on current os
  */
 def call(String command, Boolean returnStatus = false, Boolean returnStdout = false) {
-    String osName = System.getProperty("os.name").toLowerCase()
-    if((osName.contains("windows"))) {
+    if(isUnix()) {
         return bat(script: "gradlew.bat $command", returnStdout:returnStdout, returnStatus:returnStatus)    
     }
     else {
