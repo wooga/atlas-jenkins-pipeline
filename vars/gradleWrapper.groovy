53733c4a369ced617ee1f87dbe9ec88c329d9480
Manfred Endres <manfred.endres@tslarusso.de>
2017-12-19 11:23:22 +0100

    Ad clean command method to remove empty -Pproperty= switches

--- vars/gradleWrapper.groovy
+++ vars/gradleWrapper.groovy
@@ -1,13 +1,18 @@
 #!/usr/bin/env groovy
 
+def cleanupCommand(String command) {
+   regex = /-P[\w\d.]+=\s/
+   command.replaceAll(regex, '')
+}
+
 /**
  * execute gradlew or gradlew.bat based on current os
  */
 def call(String command, Boolean returnStatus = false, Boolean returnStdout = false) {
     if(isUnix()) {
-        return sh(script: "./gradlew $command", returnStdout:returnStdout, returnStatus:returnStatus)    
+        return sh(script: "./gradlew ${cleanupCommand(command)}", returnStdout:returnStdout, returnStatus:returnStatus)
     }
     else {
-        return bat(script: "gradlew.bat $command", returnStdout:returnStdout, returnStatus:returnStatus)    
-    }    
+        return bat(script: "gradlew.bat ${cleanupCommand(command)}", returnStdout:returnStdout, returnStatus:returnStatus)
+    }
 }
