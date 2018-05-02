1f3b44e051cd43bb74c9eaba49139544046a1708
Manfred Endres <manfred.endres@tslarusso.de>
2018-05-02 10:35:57 +0200

    Set UNITY_LOG_CATEGORY for parallel builds

--- vars/buildWDK.groovy
+++ vars/buildWDK.groovy
@@ -72,7 +72,8 @@ def call(Map config = [unityVersions:[]]) {
             }
 
             environment {
-              UNITY_PATH = "${APPLICATIONS_HOME}/Unity-${unityVersions[0]}/${UNITY_EXEC_PACKAGE_PATH}"
+              UNITY_PATH         = "${APPLICATIONS_HOME}/Unity-${unityVersions[0]}/${UNITY_EXEC_PACKAGE_PATH}"
+              UNITY_LOG_CATEGORY = "build"
             }
 
             steps {
@@ -118,14 +119,15 @@ def call(Map config = [unityVersions:[]]) {
         }
 
         environment {
-          GRGIT           = credentials('github_up')
-          GRGIT_USER      = "${GRGIT_USR}"
-          GRGIT_PASS      = "${GRGIT_PSW}"
-          GITHUB_LOGIN    = "${GRGIT_USR}"
-          GITHUB_PASSWORD = "${GRGIT_PSW}"
-          NUGET_KEY       = credentials('artifactory_deploy')
-          nugetkey        = "${NUGET_KEY}"
-          UNITY_PATH = "${APPLICATIONS_HOME}/Unity-${unityVersions[0]}/${UNITY_EXEC_PACKAGE_PATH}"
+          GRGIT              = credentials('github_up')
+          GRGIT_USER         = "${GRGIT_USR}"
+          GRGIT_PASS         = "${GRGIT_PSW}"
+          GITHUB_LOGIN       = "${GRGIT_USR}"
+          GITHUB_PASSWORD    = "${GRGIT_PSW}"
+          NUGET_KEY          = credentials('artifactory_deploy')
+          nugetkey           = "${NUGET_KEY}"
+          UNITY_PATH         = "${APPLICATIONS_HOME}/Unity-${unityVersions[0]}/${UNITY_EXEC_PACKAGE_PATH}"
+          UNITY_LOG_CATEGORY = "build"
         }
 
         steps {
@@ -157,7 +159,7 @@ def transformIntoCheckStep(version) {
     node("unity_${version}e && atlas && primary") {
       try {
         checkout scm
-        withEnv(["UNITY_PATH=${APPLICATIONS_HOME}/Unity-${version}/${UNITY_EXEC_PACKAGE_PATH}"]) {
+        withEnv(["UNITY_PATH=${APPLICATIONS_HOME}/Unity-${version}/${UNITY_EXEC_PACKAGE_PATH}", "UNITY_LOG_CATEGORY=check-${version}"]) {
           unstash 'setup_w'
           gradleWrapper "-Prelease.stage=${params.RELEASE_TYPE.trim()} -Prelease.scope=$params.RELEASE_SCOPE check"
         }
