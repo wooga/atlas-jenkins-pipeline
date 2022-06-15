9b853a9bd2031db329d58e226957ec55ea1d84ae
Joaquim Alvino de Mesquita Neto <joaquim.neto@wooga.net>
2022-06-15 11:54:23 +0200

    [ATLAS-831] Adds buildLocalGradlePlugin pipeline using inner plugin as sample (#129)
    Adds new pipeline for local plugin project tests, vars/buildLocalGradlePlugin.groovy. Its set to be triggered whenever a change in a given checkDirectory is passed through, and then runs in the same framework as the other java libraries (vars/javaLibs.groovy), except it never publishes anything. If checkDirectory is empty it will assume that the git root is the location of the local project.
    
    This new pipeline was tested and is applied in this repository local plugin atlas-jenkins-pipeline-gradle. This PR also updates the local plugin build.gradle file to use the lastest version of atlas-plugins.

---
+++ vars/buildLocalGradlePlugin.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/env groovy
+
+import net.wooga.jenkins.pipeline.config.JavaConfig
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                    //
+// Step buildGradlePlugin                                                                                             //
+//                                                                                                                    //
+//                                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def call(Map configMap = [:]) {
+    def checkDir = configMap.checkDir?: "."
+    javaLibs(configMap) { stages ->
+        stages.check = { check, params, config ->
+            check.when = {
+                anyOf {
+                    changeset comparator: 'GLOB', pattern: "${checkDir}/**"
+                    isRestartedRun()
+                    triggeredBy 'UserIdCause'
+                }
+            }
+        }
+        stages.publish = { publish, params, JavaConfig config ->
+            publish.when = { false }
+        }
+    }
+}
