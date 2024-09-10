48ac34b282c9a9a8e1f7c18a68cc0e76c6439cd0
Gleb <>
2024-09-10 15:29:36 +0200

    improve build steps

--- vars/buildUnityWdkV3.groovy
+++ vars/buildUnityWdkV3.groovy
@@ -231,27 +231,7 @@ def call(Map configMap = [unityVersions: []]) {
             steps {
               script {
                 script {
-                  parallel {
-                    def stepsForParallel = [:]
-
-                    if (hasToBuildPaket) {
-                      stepsForParallel['paket'] = {
-                        withEnv(["UNITY_PACKAGE_MANAGER=paket"]) {
-                          parallel checkSteps(config, "paket check unity ", "paket_setup_w")
-                        }
-                      }
-                    }
-
-                    if (hasToBuildUpm) {
-                      stepsForParallel['upm'] = {
-                        withEnv(["UNITY_PACKAGE_MANAGER=upm"]) {
-                          parallel checkSteps(config, "upm check unity ", "upm_setup_w")
-                        }
-                      }
-                    }
-
-                    return stepsForParallel
-                  }
+                  parallel collectStepsForRequestedPackageTypes(config, hasToBuildUpm, hasToBuildPaket)
                 }
               }
             }
@@ -309,6 +289,28 @@ def call(Map configMap = [unityVersions: []]) {
       }
     }
   }
+}
+
+def collectStepsForRequestedPackageTypes(WDKConfig config, boolean hasToBuildUpm, boolean hasToBuildPaket) {
+  def stepsForParallel = [:]
+
+  if (hasToBuildUpm) {
+    stepsForParallel['upm'] = {
+      withEnv(["UNITY_PACKAGE_MANAGER=upm"]) {
+        parallel checkSteps(config, "upm check unity ", "upm_setup_w")
+      }
+    }
+  }
+
+  if (hasToBuildPaket) {
+    stepsForParallel['paket'] = {
+      withEnv(["UNITY_PACKAGE_MANAGER=paket"]) {
+        parallel checkSteps(config, "paket check unity ", "paket_setup_w")
+      }
+    }
+  }
+
+  return stepsForParallel
 }
 
 def checkSteps(WDKConfig config, String parallelChecksPrefix, String setupStashId) {
