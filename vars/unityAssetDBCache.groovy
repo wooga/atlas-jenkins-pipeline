248c3d926019bf9219bcd25d67cde86ae8121403
Joaquim Neto <joaquim.neto@wooga.net>
2025-06-05 15:01:40 +0200

    add unity library cache implementation

---
+++ vars/unityAssetDBCache.groovy
@@ -0,0 +1,27 @@
+import net.wooga.jenkins.pipeline.cache.Cache
+
+def call(String projectName, String unityProjectFolder=null, boolean cachePerBranch = true) {
+    unityProjectFolder = unityProjectFolder ?: projectName
+
+    def cache = new Cache(this, projectName, cachePerBranch)
+    def libraryFolder = "$unityProjectFolder/Library"
+    def assetArtifacts = "$libraryFolder/Artifacts"
+    def artifactDB = "$libraryFolder/ArtifactDB"
+    def sourceAssetsDB = "$libraryFolder/SourceAssetDB"
+
+    return [
+            renewUnityLibraryCache   : { long cacheMaxAgeMs, Closure generateAssets ->
+                cache.renewProjectCache(libraryFolder, cacheMaxAgeMs, artifactDB, generateAssets)
+            },
+            getCacheLocation         : { ->
+                cache.getCacheLocation()
+            },
+            loadUnityAssetDBFromCache: { ->
+                if(cache.hasFiles([assetArtifacts, artifactDB, sourceAssetsDB])) {
+                    cache.loadFromCache(assetArtifacts)
+                    cache.loadFromCache(artifactDB)
+                    cache.loadFromCache(sourceAssetsDB)
+                }
+            }
+    ]
+}
