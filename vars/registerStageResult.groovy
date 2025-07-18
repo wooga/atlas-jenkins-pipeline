a94419c70245d8ebc5872961cc3b9a34c08ed151
Joaquim Neto <joaquim.neto@wooga.net>
2025-07-18 17:15:32 +0200

    create registerStageRresult step

--- vars/registerStageResult.groovy
+++ vars/registerStageResult.groovy
@@ -1,30 +1,10 @@
-def call() {
-    final Map<String, String> stageResults = [:]
-    final def STAGE_FAILED_RESULT = "failed"
-    final def STAGE_SUCCEEDED_RESULT = "succeeded"
 
-    return new Object() {
-        def stageResults() {
-            return stageResults
-        }
-        def successLabel() {
-            return STAGE_SUCCEEDED_RESULT
-        }
-        def failedLabel() {
-            return STAGE_FAILED_RESULT
-        }
-        def run(String stageName=env.STAGE_NAME, Closure action) {
-            if(stageName == null) {
-                action()
-                return
-            }
-            def previouslyFailed = (stageResults[stageName] == STAGE_FAILED_RESULT)
-            stageResults[stageName] = STAGE_FAILED_RESULT
-            action()
-            if(previouslyFailed == false) {
-                stageResults[stageName] = STAGE_SUCCEEDED_RESULT
-            }
-        }
-    }
+def call(def stageResultRegisterer, String stageName, Closure action) {
+    return stageResultRegisterer.runWithName(stageName, action)
 }
+
+def call(def stageResultRegisterer, Closure action) {
+    return stageResultRegisterer.run(action)
+}
+
 
